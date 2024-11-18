97be7bfa9ca738f23f2110e5f16cb5485ab88f71
Murtaki <laravel1010@gmail.com>
2024-11-18 15:18:02 +0700

    todolist dengan reac

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import './index.css';
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      text:"Hello word"
+    }
+  ]);
+  const [inputNotes, setInputNote] = useState('');
+  function handleInput(e) {
+    setInputNote(e.target.value);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (inputNotes.trim() !== "") {
+      const newNote={
+        id: new Date().getTime(),
+        text: inputNotes,
+    }
+    setNotes([...notes,newNote])
+      setInputNote('');
+    }
+  }
+  function handleDelete(id) {
+    setNotes(notes.filter(note=> note.id !==id))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+          <h1 className="">Note</h1>
+          <form onSubmit={handleSubmit} className="note-input">
+            <input className="" type='text' value={inputNotes} onChange={handleInput}  placeholder='Add a note' />
+            <button type='submit ' className="">Add</button>
+          </form>
+          <ul className="note-list">
+            {notes.map((note , index) => (
+              <li className="" key={index}>
+              {note.text}
+                <button onClick={()=>handleDelete(note.id) } className="">Hapus</button>
+              </li>
+            ))}
+          </ul>
+      </div>
+    </>
   );
 }
-
 export default App;
